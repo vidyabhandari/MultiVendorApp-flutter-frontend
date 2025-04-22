79addb1b623556c048e5b79ac006c8103baea2f3
vidyabhandari <vidyabhandari2804@gmail.com>
2025-04-22 15:57:54 +0530

    all fastest foods page added

--- lib/views/home/all_fastest_foods_page.dart
+++ lib/views/home/all_fastest_foods_page.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:my_firstapp/common/app_style.dart';
+import 'package:my_firstapp/common/back_ground_container.dart';
 import 'package:my_firstapp/common/reusable_text.dart';
 import 'package:my_firstapp/constants/constants.dart';
+import 'package:my_firstapp/constants/uidata.dart';
+import 'package:my_firstapp/views/home/widgets/food_tile.dart';
 
 class AllFastestFoodsPage extends StatelessWidget {
   const AllFastestFoodsPage({super.key});
@@ -12,10 +16,23 @@ class AllFastestFoodsPage extends StatelessWidget {
       appBar: AppBar(
         elevation: 0.3,
         backgroundColor: kOffWhite,
-        title: ReusableText(text: "All FastestFoods", 
-        style: appStyle(13, kGray, FontWeight.w600,0)),
+        title: ReusableText(
+          text: "All FastestFoods",
+          style: appStyle(13, kGray, FontWeight.w600, 0),
+        ),
       ),
-      body: const Center(child: Text("All Fastest Food")),
+      body: BackGroundContainer(
+        color: Colors.white,
+        child: Padding(
+          padding: EdgeInsets.all(12.h),
+          child: ListView(
+            children: List.generate(foods.length, (i) {
+              var food = foods[i];
+              return FoodTile(food: food);
+            }),
+          ),
+        ),
+      ),
     );
   }
 }
