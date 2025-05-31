67fef647790f800fc5e71c6ee8c9c529a1448a73
vidyabhandari <vidyabhandari2804@gmail.com>
2025-05-31 16:07:41 +0530

    food page updated

--- lib/views/food/food_page.dart
+++ lib/views/food/food_page.dart
@@ -4,7 +4,9 @@ import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_vector_icons/flutter_vector_icons.dart';
 import 'package:get/get.dart';
+import 'package:my_firstapp/common/app_style.dart';
 import 'package:my_firstapp/common/custom_button.dart';
+import 'package:my_firstapp/common/reusable_text.dart';
 import 'package:my_firstapp/constants/constants.dart';
 import 'package:my_firstapp/hooks/fetch_restaurant.dart';
 import 'package:my_firstapp/models/foods_model.dart';
@@ -111,6 +113,75 @@ class _FoodPageState extends State<FoodPage> {
                   ),
                 ),
               ],
+            ),
+          ),
+
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 12.w),
+            child: SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(height: 10.h),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      ReusableText(
+                        text: widget.food.title,
+                        style: appStyle(18, kDark, FontWeight.w600, 0),
+                      ),
+                      ReusableText(
+                        text: "\$ ${widget.food.price.toStringAsFixed(2)}",
+                        style: appStyle(18, kPrimary, FontWeight.w600, 0),
+                      ),
+                    ],
+                  ),
+
+                  SizedBox(height: 5.h),
+                  Text(
+                    widget.food.description,
+                    textAlign: TextAlign.justify,
+                    style: appStyle(12, kGray, FontWeight.w400, 0),
+                  ),
+
+                  SizedBox(height: 5.h),
+                  SizedBox(
+                    height: 18.h,
+                    child: ListView(
+                      scrollDirection: Axis.horizontal,
+                      children: List.generate(widget.food.foodTags.length, (
+                        index,
+                      ) {
+                        final tag = widget.food.foodTags[index];
+                        return Container(
+                          margin: EdgeInsets.only(right: 5.w),
+                          decoration: BoxDecoration(
+                            color: kPrimary,
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(15.r),
+                            ),
+                          ),
+                          child: Center(
+                            child: Padding(
+                              padding: EdgeInsets.symmetric(horizontal: 6.w),
+                              child: ReusableText(
+                                text: tag,
+                                style: appStyle(11, kWhite, FontWeight.w400, 0),
+                              ),
+                            ),
+                          ),
+                        );
+                      }),
+                    ),
+                  ),
+
+                  SizedBox(height: 15.h),
+                  ReusableText(
+                    text: "Additives and Toppings",
+                    style: appStyle(18, kDark, FontWeight.w400, 0),
+                  ),
+                ],
+              ),
             ),
           ),
         ],
