356364722d733f0ee2fcf543d80b9f4093a6b2cd
vidyabhandari <vidyabhandari2804@gmail.com>
2025-06-02 11:42:44 +0530

    image added to restaurant page

--- lib/views/restaurants/restaurant_page.dart
+++ lib/views/restaurants/restaurant_page.dart
@@ -1,4 +1,7 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:my_firstapp/constants/constants.dart';
 import 'package:my_firstapp/models/restaurants_model.dart';
 
 class RestaurantPage extends StatefulWidget {
@@ -12,6 +15,28 @@ class RestaurantPage extends StatefulWidget {
 class _RestaurantPageState extends State<RestaurantPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(), body: Container());
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        backgroundColor: kLightWhite,
+        body: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            Stack(
+              children: [
+                SizedBox(
+                  height: 230.h,
+                  width: width,
+                  child: CachedNetworkImage(
+                    fit: BoxFit.cover,
+                    imageUrl: widget.restaurant!.imageUrl,
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
