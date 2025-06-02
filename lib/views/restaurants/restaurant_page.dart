d067c03c554ea59e30090e243fd35319408b7889
vidyabhandari <vidyabhandari2804@gmail.com>
2025-06-02 11:57:31 +0530

    added navigation to rating bar

--- lib/views/restaurants/restaurant_page.dart
+++ lib/views/restaurants/restaurant_page.dart
@@ -1,8 +1,14 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/get.dart';
+import 'package:get/get_core/src/get_main.dart';
+import 'package:my_firstapp/common/custom_button.dart';
 import 'package:my_firstapp/constants/constants.dart';
 import 'package:my_firstapp/models/restaurants_model.dart';
+
+import 'rating_page.dart';
 
 class RestaurantPage extends StatefulWidget {
   const RestaurantPage({super.key, required this.restaurant});
@@ -30,6 +36,45 @@ class _RestaurantPageState extends State<RestaurantPage> {
                   child: CachedNetworkImage(
                     fit: BoxFit.cover,
                     imageUrl: widget.restaurant!.imageUrl,
+                  ),
+                ),
+
+                Positioned(
+                  bottom: 0,
+                  child: Container(
+                    padding: EdgeInsets.symmetric(horizontal: 8.w),
+                    width: width,
+                    height: 40.h,
+                    decoration: BoxDecoration(
+                      color: kPrimary.withOpacity(0.4),
+                      borderRadius: BorderRadius.only(
+                        topRight: Radius.circular(8.r),
+                        topLeft: Radius.circular(8.r),
+                      ),
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        RatingBarIndicator(
+                          itemCount: 5,
+                          itemSize: 25,
+                          rating: widget.restaurant!.rating.toDouble(),
+                          itemBuilder:
+                              (context, i) =>
+                                  const Icon(Icons.star, color: Colors.yellow),
+                        ),
+
+                        CustomButton(
+                          onTap: () {
+                            Get.to(() => const RatingPage());
+                          },
+                          radius: 0,
+                          btnColor: kSecondary,
+                          btnWidth: width / 3,
+                          text: "Rate Restaurant",
+                        ),
+                      ],
+                    ),
                   ),
                 ),
               ],
